93fd7753b21913acb8f3d4de93b96d20f5b982cb
Talha Bin Rizwan <talha.rizwan@percona.com>
2023-11-22 11:07:51 +0500

    PMM-7 Running docker in image.

--- pmm/pmm2-ami.groovy
+++ pmm/pmm2-ami.groovy
@@ -17,9 +17,9 @@ pipeline {
         disableConcurrentBuilds()
         parallelsAlwaysFailFast()
     }
-    triggers {
-        upstream upstreamProjects: 'pmm2-server-autobuild', threshold: hudson.model.Result.SUCCESS
-    }
+    // triggers {
+    //     upstream upstreamProjects: 'pmm2-server-autobuild', threshold: hudson.model.Result.SUCCESS
+    // }
     stages {
         stage('Prepare') {
             steps {
@@ -33,52 +33,42 @@ pipeline {
                 ])
             }
         }
-        stage('Build Release Candidate Images') {
-            when {
-                expression { env.RELEASE_CANDIDATE == "yes" }
-            }
-            parallel {
-                stage('Build Image Release Candidate EL7') {
-                    steps {
-                        dir("build") {
-                            sh 'make pmm2-ami-rc'
-                        }
-                        script {
-                            env.AMI_ID_EL7 = sh(script: "jq -r '.builds[-1].artifact_id' build/manifest.json | cut -d ':' -f2", returnStdout: true)
-                        }
-                    }
-                }
-                stage('Build Image Release Candidate EL9') {
-                    steps {
-                        dir("build") {
-                            sh 'make pmm2-ami-el9-rc'
-                        }
-                        script {
-                            env.AMI_ID = sh(script: "jq -r '.builds[-1].artifact_id' build/manifest.json | cut -d ':' -f2", returnStdout: true)
-                        }
-                    }
-                }
-            }
-        }
+        // stage('Build Release Candidate Images') {
+        //     when {
+        //         expression { env.RELEASE_CANDIDATE == "yes" }
+        //     }
+        //     parallel {
+        //         stage('Build Image Release Candidate EL7') {
+        //             steps {
+        //                 dir("build") {
+        //                     sh 'make pmm2-ami-rc'
+        //                 }
+        //                 script {
+        //                     env.AMI_ID_EL7 = sh(script: "jq -r '.builds[-1].artifact_id' build/manifest.json | cut -d ':' -f2", returnStdout: true)
+        //                 }
+        //             }
+        //         }
+        //         stage('Build Image Release Candidate EL9') {
+        //             steps {
+        //                 dir("build") {
+        //                     sh 'make pmm2-ami-el9-rc'
+        //                 }
+        //                 script {
+        //                     env.AMI_ID = sh(script: "jq -r '.builds[-1].artifact_id' build/manifest.json | cut -d ':' -f2", returnStdout: true)
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
         stage('Build Images Dev-Latest') {
             when {
                 expression { env.RELEASE_CANDIDATE == "no" }
             }
             parallel {
-                stage('Build Image Dev-Latest EL7') {
-                    steps {
-                        dir("build") {
-                            sh 'make pmm2-ami'
-                        }
-                        script {
-                            env.AMI_ID_EL7 = sh(script: "jq -r '.builds[-1].artifact_id' build/manifest.json | cut -d ':' -f2", returnStdout: true)
-                        }
-                    }
-                }
                 stage('Build Image Dev-Latest EL9') {
                     steps {
                         dir("build") {
-                            sh 'make pmm2-ami-el9'
+                            sh 'make pmm2-ami'
                         }
                         script {
                             env.AMI_ID = sh(script: "jq -r '.builds[-1].artifact_id' build/manifest.json | cut -d ':' -f2", returnStdout: true)
@@ -89,13 +79,6 @@ pipeline {
         }
         stage('Run PMM AMI UI tests'){
             parallel {
-                stage('Run PMM AMI UI tests EL7'){
-                    steps{
-                        script {
-                            build job: 'pmm2-ami-test', parameters: [ string(name: 'AMI_ID', value: env.AMI_ID_EL7) ]
-                        }
-                    }
-                }
                 stage('Run PMM AMI UI tests EL9'){
                     steps{
                         script {
@@ -111,16 +94,16 @@ pipeline {
             script {
                 if (params.RELEASE_CANDIDATE == "yes") {
                     currentBuild.description = "Release Candidate Build - EL7: ${env.AMI_ID_EL7}, EL9: ${env.AMI_ID}"
-                    slackSend botUser: true, channel: '#pmm-qa', color: '#00FF00', message: "[${JOB_NAME}]: ${BUILD_URL} Release Candidate build finished - EL7: ${env.AMI_ID_EL7}, EL9: ${env.AMI_ID}"
+                    // slackSend botUser: true, channel: '#pmm-qa', color: '#00FF00', message: "[${JOB_NAME}]: ${BUILD_URL} Release Candidate build finished - EL7: ${env.AMI_ID_EL7}, EL9: ${env.AMI_ID}"
                 } else {
                     currentBuild.description = "AMI Instance ID - EL7: ${env.AMI_ID_EL7}, EL9: ${env.AMI_ID}"
-                    slackSend botUser: true, channel: '#pmm-ci', color: '#00FF00', message: "[${JOB_NAME}]: build ${BUILD_URL} finished - EL7: ${env.AMI_ID_EL7}, EL9: ${env.AMI_ID}"
+                    // slackSend botUser: true, channel: '#pmm-ci', color: '#00FF00', message: "[${JOB_NAME}]: build ${BUILD_URL} finished - EL7: ${env.AMI_ID_EL7}, EL9: ${env.AMI_ID}"
                 }
             }
         }
         failure {
             echo "Pipeline failed"
-            slackSend botUser: true, channel: '#pmm-ci', color: '#FF0000', message: "[${JOB_NAME}]: build ${BUILD_URL} failed"
+            // slackSend botUser: true, channel: '#pmm-ci', color: '#FF0000', message: "[${JOB_NAME}]: build ${BUILD_URL} failed"
         }
     }
 }
