120e4fe27018c06675fb092901cb5edf727a7b0f
Naeem Akhter <naeem.akhter@percona.com>
2023-12-01 17:20:21 +0500

    Adding molecule setup file to use python39 for ppg test workflows. Updated ppg workflows to use PPG installMoleculePPG function.

---
+++ vars/installMoleculePPG.groovy
@@ -0,0 +1,13 @@
+def call() {
+        sh """
+            sudo yum install -y python39 gcc python3-pip python3-devel libselinux-python3
+            sudo yum remove ansible -y
+            python3 -m venv virtenv
+            . virtenv/bin/activate
+            python3 --version
+            python3 -m pip install --upgrade pip
+            python3 -m pip install --upgrade setuptools
+            python3 -m pip install --upgrade setuptools-rust
+            python3 -m pip install --upgrade molecule==3.3.0 pytest-testinfra pytest molecule-ec2==0.3 molecule[ansible] boto3 boto
+        """
+}
